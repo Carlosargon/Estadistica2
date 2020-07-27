e2c72977435d563622c60706f02dfc46d5795fe8
CatalinaVL <catalina.vidal@ug.uchile.cl>
2020-07-26 22:04:58 -0400

    Avance en ambas sintaxis, error sjPlot

--- proc/Sintaxis Preparacion.R
+++ proc/Sintaxis Preparacion.R
@@ -1,72 +1,107 @@
-#Cargar librerias 
+
+#         CODIGO DE PREPARACION 
+
+# Cargar librerias 
 pacman::p_load(readxl, dplyr,stargazer, car, sjlabelled, summarytools)
 options(scipen=999)
 
-#------- BASE DE DATOS N°1 -------
-#Cargar base de datos
-SIMCE2018 <- read_excel("input/simce2m2018_rbd_publica_final.xlsx")
+#---------BASE DE DATOS N°1 -------
+# Cargar base de datos
+SIMCE2018 <- read_excel("input/data/original/simce2m2018_rbd_publica_final.xlsx")
 
-#-----Explorar base-----
+#-----Explorar base
 #Tipo de base
 class(SIMCE2018)
 #Dimension de la base
 dim(SIMCE2018)
-#Nombres en las bases 
+#Nombres en las variables 
 names(SIMCE2018)
 #Visualizar
 View(SIMCE2018)
 
-#Seleccionar variables
-proc_SIMCE <- SIMCE2018 %>% select("nom_reg_rbd", "nom_rbd", "cod_grupo", 
-                                   "prom_lect2m_rbd", "prom_mate2m_rbd", "prom_nat2m_rbd") 
-
-View(proc_SIMCE)
-
-
-#Renombrar variables
-proc_SIMCE <- proc_SIMCE %>% rename("GSE" = cod_grupo)
-proc_SIMCE <- proc_SIMCE %>% rename("region" = nom_reg_rbd)
-proc_SIMCE <- proc_SIMCE %>% rename("establecimiento" = nom_rbd)
-proc_SIMCE <- proc_SIMCE %>% rename("prom_lectura" = prom_lect2m_rbd)
-proc_SIMCE <- proc_SIMCE %>% rename("prom_matematica" = prom_mate2m_rbd)
-proc_SIMCE <- proc_SIMCE %>% rename("prom_csociales" = prom_nat2m_rbd)
-
-
-
 #----------BASE DE DATOS N°2---------
 
-#Cargar base de datos
-IDPS2018 <- read_excel("input/idps_2m2018.xlsx")
+# Cargar base de datos
+IDPS2018 <- read_excel("input/data/original/idps_2m2018.xlsx")
 
-#Explorar base
+#-----Explorar base
+#Tipo de base
 class(IDPS2018)
+#Dimension de la base
 dim(IDPS2018)
+#Nombres de las variables
 names(IDPS2018)
+#Visualizar
 View(IDPS2018)
 
-#Seleccionar variables
-proc_IDPS <- IDPS2018 %>% select("NOM_RBD", "NOM_REGI_N", "ind_cc_rbd")
-View(proc_IDPS)
-
-#Renombrar variables
-proc_IDPS <- proc_IDPS %>% rename("establecimiento" = NOM_RBD)
-proc_IDPS <- proc_IDPS %>% rename("region" = NOM_REGI_N)
-proc_IDPS <- proc_IDPS %>% rename("ind_clima_conv" = ind_cc_rbd)
-
+# Renombrar variable RBD
+IDPS2018 <- IDPS2018 %>% rename("rbd" = RBD)
 
 #-----Union bases de datos-----
-SIM_IDPS <- cbind(proc_IDPS, proc_SIMCE)
 
-#----Casos perdidos
+SIM_IDPS<- left_join(x = IDPS2018,y = SIMCE2018, by ="rbd")
+View(SIM_IDPS)
+
+# Seleccionar variables
+proc_SIM_IDPS <- SIM_IDPS %>% select("nom_reg_rbd","cod_grupo.y",  "ind_cc_rbd", "ind_am_rbd",
+                                   "prom_lect2m_rbd", "prom_mate2m_rbd", "prom_nat2m_rbd") 
+
+
+View(proc_SIM_IDPS)
+
+proc_SIM_IDPS2 <- iris[iris$Species == "setosa",]
+
+# Renombrar variables
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("GSE" = cod_grupo.y)
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("prom_lectura" = prom_lect2m_rbd)
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("prom_matematica" = prom_mate2m_rbd)
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("prom_csociales" = prom_nat2m_rbd)
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("ind_clima" = ind_cc_rbd)
+proc_SIM_IDPS <- proc_SIM_IDPS %>% rename("ind_autoestima" = ind_am_rbd)
+
+# Verificar
+View(proc_SIM_IDPS)
+
+#------Casos perdidos 
+
 #Dimension base
-dim(SIM_IDPS)
+dim(proc_SIM_IDPS)
 #Contar casos perdidos 
-sum(is.na(SIM_IDPS))
+sum(is.na(proc_SIM_IDPS))
 #identificar casos perdidos 
-which(is.na(SIM_IDPS))
+which(is.na(proc_SIM_IDPS))
 #Omitir casos perdidos
-SIM_IDPS <- na.omit(SIM_IDPS)
+proc_SIM_IDPS <- na.omit(proc_SIM_IDPS)
 #Rectificar
-sum(is.na(SIM_IDPS))
+sum(is.na(proc_SIM_IDPS))
+
+# Verificar etiquetas de la base
+sjlabelled::get_label(proc_SIM_IDPS)
+
+# Generar etiquetas
+proc_SIM_IDPS$GSE <- set_label(x = proc_SIM_IDPS$GSE,label = "Grupo socioeconómico")
+proc_SIM_IDPS$ind_clima <- set_label(x = proc_SIM_IDPS$ind_clima,label = "Puntaje Indicador Clima de Convivencia escolar del colegio")
+proc_SIM_IDPS$ind_autoestima <- set_label(x = proc_SIM_IDPS$ind_autoestima,label = "Puntaje Indicador Autoestima académica y motivación escolar del colegio")
+proc_SIM_IDPS$prom_lectura <- set_label(x = proc_SIM_IDPS$prom_lectura,label = "Puntaje promedio del establecimiento en Lectura")
+proc_SIM_IDPS$prom_matematica <- set_label(x = proc_SIM_IDPS$prom_matematica,label = "Puntaje promedio del establecimiento en Matemática")
+proc_SIM_IDPS$prom_csociales <- set_label(x = proc_SIM_IDPS$prom_csociales,label = "Puntaje promedio del establecimiento en Ciencias Sociales")
+
+# Verificar etiquetas 
+get_label(proc_SIM_IDPS$GSE)
+get_label(proc_SIM_IDPS$ind_clima)
+get_label(proc_SIM_IDPS$ind_autoestima)
+get_label(proc_SIM_IDPS$prom_lectura)
+get_label(proc_SIM_IDPS$prom_matematica)
+get_label(proc_SIM_IDPS$prom_csociales)
+
+# Tabla estadisticos descriptivos
+stargazer(proc_SIM_IDPS,type = "text")
+
+dfSummary(proc_SIM_IDPS, plain.ascii = FALSE)
+view(dfSummary(proc_SIM_IDPS, headings=FALSE))
+
+# Guardar base de datos procesados 
+save(proc_SIM_IDPS,file= "input/data/proc/proc_SIM_IDPS.RData" )
+
 
 
---
+++ proc/Sintaxis analisis.R
@@ -0,0 +1,70 @@
+#           CODIGO DE ANALISIS
+
+#Cargar librerias 
+pacman::p_load(dplyr, sjmisc, summarytools, sjPlot, ggplot2, webshot,
+               ggpubr, gridExtra, texreg)
+webshot::install_phantomjs( force = T)
+
+# Abrir base de datos 
+load(file= "input/data/proc/proc_SIM_IDPS.RData")
+
+# Tabla estadisticos descriptivos 
+view(dfSummary(proc_SIM_IDPS, headings = FALSE, method = 'render'), file = "output/tablas/tabla1.html")
+webshot("output/tablas/tabla1.html","output/tablas/tabla1.png")
+
+#Graficar los predictores y su relacion con variable independiente
+
+gclima <- ggscatter(proc_SIM_IDPS, x = "ind_clima", y = "GSE",
+                  shape = 21, size = 3, # Forma y tamaño de puntos
+                  add = "reg.line", #Agregar recta de regresion
+                  cor.coef = TRUE)# Agregar coeficiente correlacion
+
+
+gautoestima <- ggscatter(proc_SIM_IDPS, x = "ind_autoestima", y = "GSE",
+                    shape = 21, size = 3, # Forma y tamaño de puntos
+                    add = "reg.line", #Agregar recta de regresion
+                    cor.coef = TRUE)# Agregar coeficiente correlacion
+
+
+glectura <- ggscatter(proc_SIM_IDPS, x = "prom_lectura", y = "GSE",
+                    shape = 21, size = 3, # Forma y tamaño de puntos
+                    add = "reg.line", #Agregar recta de regresion
+                    cor.coef = TRUE)# Agregar coeficiente correlacion
+
+
+gmatematica <- ggscatter(proc_SIM_IDPS, x = "prom_matematica", y = "GSE",
+                    shape = 21, size = 3, # Forma y tamaño de puntos
+                    add = "reg.line", #Agregar recta de regresion
+                    cor.coef = TRUE)# Agregar coeficiente correlacion
+
+gcsociales <- ggscatter(proc_SIM_IDPS, x = "prom_csociales", y = "GSE",
+                        shape = 21, size = 3, # Forma y tamaño de puntos
+                        add = "reg.line", #Agregar recta de regresion
+                        cor.coef = TRUE)# Agregar coeficiente correlacion
+
+#Unir graficos
+grid.arrange(gclima, gautoestima, glectura, gmatematica, gcsociales, nrow = 1)
+
+
+#-------------- MODELO DE REGRESION MULTIPLE --------------
+
+#Regresiones simples
+clima_models<-lm(GSE ~ ind_clima, data=proc_SIM_IDPS)
+autoestima_models<-lm(GSE ~ ind_autoestima, data=proc_SIM_IDPS)
+lectura_models<-lm(GSE ~ prom_lectura, data=proc_SIM_IDPS)
+matematica_models<-lm(GSE ~ prom_matematica, data=proc_SIM_IDPS)
+csociales_models<-lm(GSE ~ prom_csociales, data=proc_SIM_IDPS)
+
+#Tabla simple de regresion multiple
+modelom <- lm(GSE ~ ind_clima + ind_autoestima + prom_lectura + prom_matematica + prom_csociales, data=proc_SIM_IDPS)
+
+#Tabla publicable de regresion multiple 
+tab_model(list(modelom), show.ci=FALSE,
+                  p.style = "asterisk", dv.labels = c("Modelo1"),
+                  string.pred = "Predictores", string.est = "β")
+
+help(package = "sjPlot")
+install_cran("sjPlot")
+install.packages()
+
+
